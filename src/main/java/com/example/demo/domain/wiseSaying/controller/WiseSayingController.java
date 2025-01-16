d40802b031f44a7f6dff4ed19efe30bc6532fb51
kozae00 <rhwogus777@gmail.com>
2025-01-16 14:10:35 +0900

    PathVariable - id

--- src/main/java/com/example/demo/domain/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/example/demo/domain/wiseSaying/controller/WiseSayingController.java
@@ -5,6 +5,7 @@ import com.example.demo.domain.wiseSaying.service.WiseSayingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -20,22 +21,17 @@ public class WiseSayingController {
         return wiseSayingService.getAllItems();
     }
 
-    @GetMapping("/wiseSaying/write")
-    public WiseSaying writeWiseSaying(String content, @RequestParam(defaultValue = "no name") String author) { // @RequestParam(...) 과 String ... 같으면 생략 가능
+    @GetMapping("/wiseSayings/write")
+    public WiseSaying writeWiseSayings(String content, @RequestParam(defaultValue = "no name") String author) { // @RequestParam(...) 과 String ... 같으면 생략 가능
         System.out.println("content = " + content);
         System.out.println("author = " + author);
 
         return wiseSayingService.write(content, author);
     }
 
-    @GetMapping("/wiseSayings/1")
-    public WiseSaying getItem1() {
-        return wiseSayingService.getItem(1).orElse(null);
-    }
-
-    @GetMapping("/wiseSayings/2")
-    public WiseSaying getItem2() {
-        return wiseSayingService.getItem(2).orElse(null);
+    @GetMapping("/wiseSayings/{id}")
+    public WiseSaying getItem1(@PathVariable("id") int id) { // @PathVariable("id") int id 변수명이 같으면 @pPathVariable(...)생략 가능
+        return wiseSayingService.getItem(id).orElse(null);
     }
 
 }
