b607152936f7a8f8b4620a2a7bc0e2088cdefe64
kozae00 <rhwogus777@gmail.com>
2025-01-16 12:04:26 +0900

    wiseSaying domain apply

--- src/main/java/com/example/demo/domain/home/controller/HomeController.java
+++ src/main/java/com/example/demo/domain/home/controller/HomeController.java
@@ -1,9 +1,7 @@
-package com.example.demo.controller;
+package com.example.demo.domain.home.controller;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
---
+++ src/main/java/com/example/demo/domain/wiseSaying/service/WiseSayingService.java
@@ -0,0 +1,21 @@
+package com.example.demo.domain.wiseSaying.service;
+
+import com.example.demo.domain.wiseSaying.entity.WiseSaying;
+import java.util.ArrayList;
+import java.util.List;
+public class WiseSayingService {
+    private List<WiseSaying> wiseSayingList = new ArrayList<>();
+    private int lastId = 0;
+    public List<WiseSaying> getAllItems() {
+        return wiseSayingList;
+    }
+    public WiseSaying write(String content, String author) {
+        WiseSaying wiseSaying = WiseSaying.builder()
+                .id(++lastId)
+                .content(content)
+                .author(author)
+                .build();
+        wiseSayingList.add(wiseSaying);
+        return wiseSaying;
+    }
+}
