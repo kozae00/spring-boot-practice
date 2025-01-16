be2b8d764f3c0916d0db7bf494b628224b570e0c
kozae00 <rhwogus777@gmail.com>
2025-01-16 09:49:55 +0900

    Merge remote changes and resolve conflicts

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,4 +1,16 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String home() {
+        return "hello";
+    }
+
 }
